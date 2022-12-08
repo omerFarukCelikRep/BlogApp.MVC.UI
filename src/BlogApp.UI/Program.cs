15d1cbaae3de7faa6a40abeb13cc71fbcb1eca19
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-09 00:40:23 +0300

    extension method call added

--- src/BlogApp.UI/Program.cs
+++ src/BlogApp.UI/Program.cs
@@ -1,24 +1,29 @@
+using BlogApp.UI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddMVCServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+
+app.UseSession();
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+//app.UseMiddleware<UserClaimsMiddleware>();
 
 app.MapControllerRoute(
     name: "default",

17e60f4071e75c873cc914209e76418252d992c5
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-11 23:55:30 +0300

    added

---
+++ src/BlogApp.UI/Models/Topics/TopicUpdateVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BlogApp.UI.Models.Topics;
+
+public class TopicUpdateVM
+{
+    public Guid Id { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    public string Name { get; set; } = string.Empty;
+
+    [Display(Name = "Thumbnail")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+    public IFormFile? ThumbnailFile { get; set; } = null!;
+
+    public string Thumbnail { get; set; } = null!;
+}

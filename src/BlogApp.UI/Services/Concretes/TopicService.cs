af35b491087918d300cbb3d368bed821e265addd
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-12 23:06:28 +0300

    CancellationToken added

--- src/BlogApp.UI/Services/Concretes/TopicService.cs
+++ src/BlogApp.UI/Services/Concretes/TopicService.cs
@@ -1,5 +1,6 @@
 ﻿using BlogApp.Core.Utilities.Results.Concrete;
 using BlogApp.Core.Utilities.Results.Interfaces;
+using BlogApp.UI.Extensions;
 using BlogApp.UI.Models.Topics;
 using BlogApp.UI.Services.Interfaces;
 using IResult = BlogApp.Core.Utilities.Results.Interfaces.IResult;
@@ -15,30 +16,52 @@ public class TopicService : ITopicService
         _httpClient = httpClient;
     }
 
-    public async Task<IDataResult<List<TopicListVM>>?> GetAll()
+    public async Task<IDataResult<List<TopicListVM>>?> GetAllAsync(CancellationToken cancellationToken = default)
     {
-        return await _httpClient.GetFromJsonAsync<DataResult<List<TopicListVM>>>("/api/v1/Topics");
+        return await _httpClient.GetFromJsonAsync<DataResult<List<TopicListVM>>>("/api/v1/Topics", cancellationToken);
     }
 
-    public async Task<IResult> AddAsync(TopicAddVM topicAddVM)
+    public async Task<IResult> AddAsync(TopicAddVM topicAddVM, CancellationToken cancellationToken = default)
     {
-        var responseMessage = await _httpClient.PostAsJsonAsync("/api/v1/Topics", topicAddVM);
-        if (responseMessage is null)
+        var response = await _httpClient.PostAsJsonAsync("/api/v1/Topics", topicAddVM,cancellationToken);
+        if (response is null)
         {
             return new ErrorResult("Giriş Başarısız"); //TODO: Magic string
         }
 
-        var response = await responseMessage.Content.ReadFromJsonAsync<DataResult<TopicAddVM>>();
-        if (!responseMessage.IsSuccessStatusCode)
+        var result = await response.Content.ReadFromJsonAsync<DataResult<TopicAddVM>>(cancellationToken:cancellationToken);
+        if (!response.IsSuccessStatusCode)
         {
-            return new ErrorResult($"{response!.Message}");
+            return new ErrorResult($"{result!.Message}");
         }
 
         return new SuccessResult();
     }
 
-    public async Task<IDataResult<TopicUpdateVM>?> GetById(Guid id)
+    public async Task<IDataResult<TopicUpdateVM>?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        return await _httpClient.GetFromJsonAsync<DataResult<TopicUpdateVM>>("/api/v1/Topics/" + id);
+        return await _httpClient.GetFromJsonAsync<DataResult<TopicUpdateVM>>("/api/v1/Topics/" + id, cancellationToken);
+    }
+
+    public async Task<IResult> UpdateAsync(TopicUpdateVM topicUpdateVM, CancellationToken cancellationToken = default)
+    {
+        if (topicUpdateVM.ThumbnailFile is not null)
+        {
+            topicUpdateVM.Thumbnail = await topicUpdateVM.ThumbnailFile.FileToStringAsync(cancellationToken);
+        }
+
+        var response = await _httpClient.PutAsJsonAsync("/api/v1/Topics", topicUpdateVM, cancellationToken);
+        if (response is null || !response.IsSuccessStatusCode)
+        {
+            return new ErrorResult("İşlem Başarısız!");
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<DataResult<TopicUpdateVM>>(cancellationToken: cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            return new ErrorResult($"{result?.Message}");
+        }
+
+        return new SuccessResult();
     }
 }

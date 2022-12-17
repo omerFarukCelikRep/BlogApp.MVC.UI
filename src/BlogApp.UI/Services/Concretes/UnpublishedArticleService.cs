4902ae778659e34a4d3006b75c35c27e9e9a274f
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-18 01:20:04 +0300

    added

---
+++ src/BlogApp.UI/Services/Concretes/UnpublishedArticleService.cs
@@ -0,0 +1,20 @@
+﻿using BlogApp.Core.Utilities.Results.Concrete;
+using BlogApp.Core.Utilities.Results.Interfaces;
+using BlogApp.UI.Models.UnpublishedArticles;
+using BlogApp.UI.Services.Interfaces;
+
+namespace BlogApp.UI.Services.Concretes;
+
+public class UnpublishedArticleService : IUnpublishedArticleService
+{
+    private readonly HttpClient _httpClient;
+    public UnpublishedArticleService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IDataResult<List<ArticleUnpublishedListVM>>?> GetAllUnpublished()
+    {
+        return await _httpClient.GetFromJsonAsync<DataResult<List<ArticleUnpublishedListVM>>>("/api/v1/Articles/Unpublished");
+    }
+}

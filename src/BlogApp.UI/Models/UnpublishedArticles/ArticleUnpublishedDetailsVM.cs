3c6d718e1172b73ee8ef91907d0ac43a9b15cbb0
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-18 01:20:51 +0300

    added

---
+++ src/BlogApp.UI/Models/UnpublishedArticles/ArticleUnpublishedDetailsVM.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.UI.Models.Topics;
+
+namespace BlogApp.UI.Models.UnpublishedArticles;
+
+public class ArticleUnpublishedDetailsVM
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string Content { get; set; } = null!;
+    public int ReadTime { get; set; }
+    public string? Thumbnail { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public List<TopicArticleDetailsVM> Topics { get; set; } = null!;
+    public string AuthorName { get; set; } = null!;
+}

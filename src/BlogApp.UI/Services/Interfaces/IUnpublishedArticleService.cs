e2cecc23cd563c7cede7fee66396f98999310524
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-19 01:42:44 +0300

    methods declared

--- src/BlogApp.UI/Services/Interfaces/IUnpublishedArticleService.cs
+++ src/BlogApp.UI/Services/Interfaces/IUnpublishedArticleService.cs
@@ -1,5 +1,11 @@
-﻿namespace BlogApp.UI.Services.Interfaces;
+﻿using BlogApp.Core.Utilities.Results.Interfaces;
+using BlogApp.UI.Models.UnpublishedArticles;
+
+namespace BlogApp.UI.Services.Interfaces;
 
 public interface IUnpublishedArticleService
 {
+    Task<IDataResult<List<ArticleUnpublishedListVM>>?> GetAllAsync();
+    Task<IDataResult<ArticleUnpublishedDetailsVM>?> GetByIdAsync(Guid articleId);
+    Task<Core.Utilities.Results.Interfaces.IResult> PublishAsync(Guid articleId);
 }

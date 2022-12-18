549ab496550b9c91ee5fd98b79731053b54a2301
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-19 01:42:12 +0300

    GetByIdAsync and PublishAsync methods added

--- src/BlogApp.UI/Services/Concretes/UnpublishedArticleService.cs
+++ src/BlogApp.UI/Services/Concretes/UnpublishedArticleService.cs
@@ -2,6 +2,7 @@
 using BlogApp.Core.Utilities.Results.Interfaces;
 using BlogApp.UI.Models.UnpublishedArticles;
 using BlogApp.UI.Services.Interfaces;
+using IResult = BlogApp.Core.Utilities.Results.Interfaces.IResult;
 
 namespace BlogApp.UI.Services.Concretes;
 
@@ -13,8 +14,25 @@ public class UnpublishedArticleService : IUnpublishedArticleService
         _httpClient = httpClient;
     }
 
-    public async Task<IDataResult<List<ArticleUnpublishedListVM>>?> GetAllUnpublished()
+    public async Task<IDataResult<List<ArticleUnpublishedListVM>>?> GetAllAsync()
     {
         return await _httpClient.GetFromJsonAsync<DataResult<List<ArticleUnpublishedListVM>>>("/api/v1/Articles/Unpublished");
+    }
+
+    public async Task<IDataResult<ArticleUnpublishedDetailsVM>?> GetByIdAsync(Guid articleId)
+    {
+        return await _httpClient.GetFromJsonAsync<DataResult<ArticleUnpublishedDetailsVM>>($"/api/v1/Articles/Unpublished/{articleId}");
+    }
+
+    public async Task<IResult> Publish(Guid articleId)
+    {
+        var responseMessage = await _httpClient.PostAsJsonAsync("/api/v1/Articles/Publish", articleId);
+        var response = await responseMessage.Content.ReadFromJsonAsync<Result>();
+        if (response is not null && !responseMessage.IsSuccessStatusCode)
+        {
+            return new ErrorResult($"{responseMessage.ReasonPhrase} - {response.Message}");
+        }
+
+        return new SuccessResult();
     }
 }

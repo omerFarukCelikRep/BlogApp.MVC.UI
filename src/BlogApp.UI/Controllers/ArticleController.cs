1310b50c0bb6ddd44f4628ebce5cc06706c98f79
Ömer Faruk Çelik <farukcelik2534@gmail.com>
2022-12-19 01:40:03 +0300

    Update action added

--- src/BlogApp.UI/Controllers/ArticleController.cs
+++ src/BlogApp.UI/Controllers/ArticleController.cs
@@ -1,5 +1,9 @@
-﻿using BlogApp.UI.Services.Interfaces;
+﻿using BlogApp.UI.Extensions;
+using BlogApp.UI.Models.Articles;
+using BlogApp.UI.Services.Concretes;
+using BlogApp.UI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BlogApp.UI.Controllers;
 public class ArticleController : BaseController
@@ -15,7 +19,7 @@ public class ArticleController : BaseController
     [HttpGet]
     public async Task<IActionResult> Index()
     {
-        var result = await _articleService.GetAllPublished();
+        var result = await _articleService.GetAllAsync();
 
         return View(result?.Data);
     }
@@ -23,15 +27,43 @@ public class ArticleController : BaseController
     [HttpGet]
     public async Task<IActionResult> ListByTopic([FromQuery(Name = "t")] string topicName)
     {
-        var result = await _articleService.GetAllPublishedByTopicName(topicName);
+        var result = await _articleService.GetAllByTopicNameAsync(topicName);
 
         return View(nameof(Index), result?.Data);
     }
 
     [HttpGet]
+    public async Task<IActionResult> Add()
+    {
+        return View(new ArticleAddVM
+        {
+            Topics = await GetTopics()
+        });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(ArticleAddVM articleAddVM)
+    {
+        if (!ModelState.IsValid)
+        {
+            articleAddVM.Topics = await GetTopics();
+            return View(articleAddVM);
+        }
+
+        var result = await _articleService.AddAsync(articleAddVM);
+        if (!result.IsSuccess)
+        {
+            ModelState.AddModelError(string.Empty, result.Message!); //TODO:Show message
+            return View(articleAddVM);
+        }
+
+        return RedirectToAction(nameof(Index),"UnpublishedArticle");
+    }
+
+    [HttpGet]
     public async Task<IActionResult> Details(Guid id)
     {
-        var result = await _articleService.GetPublishedById(id);
+        var result = await _articleService.GetByIdAsync(id);
         if (!result!.IsSuccess)
         {
             ModelState.AddModelError(string.Empty, result.Message!);
@@ -39,5 +71,60 @@ public class ArticleController : BaseController
         }
 
         return View(result.Data);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Update([FromServices] IUnpublishedArticleService unpublishedArticleService,Guid id)
+    {
+        var result = await unpublishedArticleService.GetByIdAsync(id);
+        if (!result!.IsSuccess)
+        {
+            ModelState.AddModelError(string.Empty, result.Message!); //TODO:Show message
+            return RedirectToAction(nameof(Index));
+        }
+
+        ArticleUpdateVM articleUpdateVM = (ArticleUpdateVM)result.Data!;
+        articleUpdateVM.Topics = await GetTopics(articleUpdateVM.TopicIds);
+        return View(articleUpdateVM);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(ArticleUpdateVM articleUpdateVM)
+    {
+        if (!ModelState.IsValid)
+        {
+            articleUpdateVM.Topics = await GetTopics(articleUpdateVM.TopicIds);
+            return View(articleUpdateVM);
+        }
+
+        if (articleUpdateVM.ThumbnailFile is not null)
+        {
+            articleUpdateVM.Thumbnail = await articleUpdateVM.ThumbnailFile.FileToStringAsync();
+        }
+
+        var result = await _articleService.UpdateAsync(articleUpdateVM);
+        if (!result!.IsSuccess)
+        {
+            ModelState.AddModelError(string.Empty, result.Message!); //TODO:Show message
+            return View(articleUpdateVM);
+        }
+
+        return RedirectToAction(nameof(Details), new { id = articleUpdateVM.Id });
+    }
+
+    private async Task<IEnumerable<SelectListItem>> GetTopics(List<Guid>? selectedIds = null)
+    {
+        var result = await _topicService.GetAllAsync();
+        if (result is null)
+        {
+            return Enumerable.Empty<SelectListItem>();
+        }
+
+        return result.Data!.ConvertAll(topic => new SelectListItem
+        {
+            Selected = selectedIds is not null && selectedIds.Any(x => x == topic.Id),
+            Text = topic.Name,
+            Value = topic.Id.ToString()
+        });
     }
 }
